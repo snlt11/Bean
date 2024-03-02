5a372147893f447bf62af40fd6025ef47f73ff8f
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-03-03 02:43:34 +0630

    Bean

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\Product\ProductController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+
+Route::get('/articles/add', [ArticleController::class, 'add']);
+
+Route::post('/articles/add', [ArticleController::class, 'create']);
+
+Route::get('/articles/delete/{id}',[ArticleController::class, 'delete']);
+
+Route::post('/comments/add', [CommentController::class, 'create']);
+
+Route::get('/comments/delete/{id}',[CommentController::class,'delete']);
+
+Auth::routes();
+
